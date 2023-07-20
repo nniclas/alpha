378ae60d400f4e51a2b2f1eecda77b0cc3bf642e
nniclas <niclas.oes@gmail.com>
2023-07-20 22:51:42 +0200

    login layout

--- src/routes/login/login.tsx
+++ src/routes/login/login.tsx
@@ -11,6 +11,29 @@ import Field from '../../lib/elements/field/field'
 import Text from '../../lib/elements/text/text'
 
 import styles from './login.module.css'
+import Button from '../../lib/elements/button/button'
+
+const Layout = (a: { bg: string; color: string; lslot: any; rslot: any }) => {
+    return (
+        <Field col rel>
+            <Field></Field>
+            <Field>
+                <Field rel h={800} bg={a.color}>
+                    <Field h={300} bg={a.bg}></Field>
+                    <Field layer>{a.lslot}</Field>
+                </Field>
+                <Field s rel w={800} h={800}>
+                    <Field class={styles.bg}></Field>
+                </Field>
+                <Field rel h={800} bg={a.color}>
+                    <Field h={600} bg={a.bg}></Field>
+                    <Field layer>{a.rslot}</Field>
+                </Field>
+            </Field>
+            <Field bg={a.color}></Field>
+        </Field>
+    )
+}
 
 export const Login: Component = () => {
     // const navigate = useNavigate()
@@ -25,12 +48,28 @@ export const Login: Component = () => {
         // }
     })
 
+    // login page covers the full window
     return (
-        <Field col rel>
-            <Field class={styles.bg} />
-            <Text accent xs>
-                login page
-            </Text>
+        <Field rel class={styles.cover}>
+            <Layout
+                bg='hsl(200, 18%, 22%)'
+                color='hsl(200, 8%, 58%)'
+                lslot={
+                    <Field aic jce style='z-index:1; margin-right:-100px'>
+                        <Text>logo</Text>
+                    </Field>
+                }
+                rslot={
+                    <Field aic jcs style='z-index:1; margin-left:-200px'>
+                        <Field col gmd>
+                            <Text>stuff</Text>
+                            <Button tertiary br md>
+                                <Text>hello there</Text>
+                            </Button>
+                        </Field>
+                    </Field>
+                }
+            />
         </Field>
     )
 }
