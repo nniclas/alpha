5134cc5302a5c930b726c0d11774a29177624371
nniclas <niclas.oes@gmail.com>
2023-07-21 16:15:20 +0200

    drafting login page and layout

--- src/routes/login/login.tsx
+++ src/routes/login/login.tsx
@@ -1,6 +1,6 @@
 import { Component, For, createEffect, createSignal, lazy } from 'solid-js'
 import { Outlet, useNavigate } from '@solidjs/router'
-// import BG from '../assets/bg2.svg?component-solid'
+import Logo from '../../assets/logo.svg?component-solid'
 
 // common css resources
 import '../../lib/styles/easing.css'
@@ -12,6 +12,7 @@ import Text from '../../lib/elements/text/text'
 
 import styles from './login.module.css'
 import Button from '../../lib/elements/button/button'
+import Textfield from '../../lib/elements/textfield/textfield'
 
 const Layout = (a: { bg: string; color: string; lslot: any; rslot: any }) => {
     return (
@@ -51,26 +52,94 @@ export const Login: Component = () => {
     // login page covers the full window
     return (
         <Field rel class={styles.cover}>
+            <Field layer jcc ais>
+                <Field s pmd h={400} aic>
+                    <Text md color='hsl(200, 12%, 42%)'>
+                        Remote operate | Monitor activity | Handle reports
+                    </Text>
+                </Field>
+            </Field>
             <Layout
                 bg='hsl(200, 18%, 22%)'
                 color='hsl(200, 8%, 58%)'
                 lslot={
-                    <Field aic jce style='z-index:1; margin-right:-280px'>
-                        <Text>•</Text>
+                    <Field aic jce gmd style='z-index:1; margin-right:-100px'>
+                        <Field s w={300} h={300}>
+                            <Logo />
+                        </Field>
                     </Field>
                 }
                 rslot={
-                    <Field aic jcs style='z-index:1; margin-left:-280px'>
+                    <Field col ais jce style='z-index:1; margin-left:-100px;'>
+                        <Field s col pmd>
+                            <Text lg color='hsl(200, 18%, 62%)'>
+                                ALPHA
+                            </Text>
+                            <Text sm color='hsl(200, 18%, 38%)'>
+                                Technical configuration tool
+                            </Text>
+                        </Field>
+                        <Field
+                            col
+                            s
+                            gsm
+                            plg
+                            style='width:340px; background:hsla(200, 18%, 20%,0.8)'
+                        >
+                            <Field s psm>
+                                <Text color='hsl(200, 18%, 62%)'>Sign in</Text>
+                            </Field>
+                            <Field s>
+                                <Textfield
+                                    primary
+                                    psm
+                                    color='hsl(200, 18%, 32%)'
+                                    value='demo@user.com'
+                                />
+                            </Field>
+                            <Field s>
+                                <Textfield
+                                    primary
+                                    psm
+                                    color='hsl(200, 18%, 32%)'
+                                    value='demo'
+                                />
+                            </Field>
+                            <Field jce>
+                                <Field s>
+                                    <Button tertiary md>
+                                        <Text color='hsl(200, 18%, 22%)'>
+                                            Sign in
+                                        </Text>
+                                    </Button>
+                                </Field>
+                            </Field>
+                        </Field>
+
+                        {/* <Text
+                            lg
+                            tertiary
+                            style='letter-spacing:2px;opacity:0.25;color:black'
+                        >
+                            ALPHA
+                        </Text> */}
                         {/* <Field col gmd>
                             <Text>stuff</Text>
                             <Button tertiary br md>
                                 <Text>hello there</Text>
                             </Button>
                         </Field> */}
-                        <Text>•</Text>
+                        {/* <Text>•</Text> */}
                     </Field>
                 }
             />
+            <Field layer jcc aie>
+                <Field s pmd>
+                    <Text color='hsl(200, 18%, 32%)' sm>
+                        nniclas © Copyright 2023
+                    </Text>
+                </Field>
+            </Field>
         </Field>
     )
 }
