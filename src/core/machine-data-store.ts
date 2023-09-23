22d7c437f6c348cc41424b6fa257e8f756e1d9eb
nniclas <niclas.oes@gmail.com>
2023-09-23 12:30:16 +0200

    machine data store to be tested

--- src/core/machine-data-store.ts
+++ src/core/machine-data-store.ts
@@ -9,80 +9,111 @@ import { randInt } from '../common/utils'
 
 // simulator!
 
-let intervals: NodeJS.Timer[]
+let intervals: (NodeJS.Timer | undefined)[][] // matrix ordered by units and measures
 
-interface MachineEntry {
+interface UnitEntry {
     measures: MachineMeasure[]
 }
 
 interface MachineMeasure {
     name: string
     reader: (lr: number) => number
+    timing: number
     value: number
 }
 
-const defaultData: MachineEntry[] = [
-    { measures: [{ name: '_', reader: () => 0, value: 0 }] },
+const defaultData: UnitEntry[] = [
+    { measures: [{ name: '_', reader: () => 0, timing: 1000, value: 0 }] },
 ]
 
 // let iSignalInterval: NodeJS.Timer, iProcessorInterval: NodeJS.Timer
 
 function createDataState() {
-    const [unitCount, setUnitCount] = createSignal<number>(1)
-    const [selected, setSelected] = createSignal<number>(0)
-    const [data, setData] = createSignal<MachineEntry[]>(defaultData)
+    // const [unitCount, setUnitCount] = createSignal<number>(1)
+    const [selectedUnit, setSelectedUnit] = createSignal<number>(0)
+    const [data, setData] = createSignal<UnitEntry[]>(defaultData) //
 
     // const [signalStrength, setSignalStrength] = createSignal<number[]>([0]) // signal strength of current monitored unit
     // const [processorUsage, setProcessorUsage] = createSignal<number[]>([0]) // processor usage of current monitored unit
     // const [batteryLevel, setBatteryLevel] = createSignal<number[]>([0]) // battery level of current monitored unit
 
+    const read = (ui: number, mi: number) => {
+        // total destructuring for immutability..
+        const d = [...data()]
+        const ud = { ...d[ui] }
+        const udm = { ...[...ud.measures][mi] }
+
+        const val = udm.reader(udm.value)
+        ud.measures[mi].value == val
+        setData(d) // update state
+    }
+
+    // set interval of a measure or all measures of a unit
+    const start = (u: number, m?: number) => {
+        const set = (m: number) => {
+            if (intervals[u][m] == undefined)
+                intervals[u][m] = setInterval(
+                    () => read(u, m),
+                    data()[u].measures[m].timing
+                )
+        }
+
+        if (!m) {
+            intervals[u].forEach((ivl, i) => set(i))
+            return
+        }
+
+        set(m)
+    }
+
+    // clear interval of a measure or all measures of a unit
+    const stop = (u: number, m?: number) => {
+        const clear = (m: number) => {
+            clearInterval(intervals[u][m])
+            intervals[u][m] = undefined
+        }
+
+        if (!m) {
+            intervals[u].forEach((ivl, i) => clear(i))
+            return
+        }
+
+        clear(m)
+    }
+
     const initialize = (
         unitCount: number,
-        names: string[],
+        mnames: string[],
         readers: (() => number)[],
-        times: number[]
+        timings: number[]
     ) => {
-        const data: MachineEntry[] = []
+        const data: UnitEntry[] = []
         for (let ui = 0; ui < unitCount; ui++) {
             data.push({
-                measures: [
-                    {
-                        name: names[ui],
-                        reader: readers[ui],
-                        value: 0,
-                    },
-                ],
+                measures: mnames.map((n, ni) => ({
+                    name: mnames[ni],
+                    reader: readers[ni],
+                    timing: timings[ni],
+                    value: 0,
+                })),
             })
 
             for (let mi = 0; mi < readers.length; mi++) {
-                // hmm this will create intervals for measure-count TIMES units...
-                // but we can have one unit active and toggle interval group on/off.. using setSelected state
-                initInterval(
-                    mi,
-                    () => {
-                        // total destructuring for immutability..
-                        const d = [...data]
-                        const ud = { ...d[ui] }
-                        const udm = { ...[...ud.measures][mi] }
-
-                        const val = udm.reader(udm.value)
-                        ud.measures[mi].value == val
-                        setData(d) // update state
-                    },
-                    times[mi]
-                )
+                // hmm this will create intervals for measure-count TIMES machines...
+                // but we can have one machine active and toggle interval group on/off.. using setSelected state
+                start(ui, mi)
             }
         }
     }
 
-    const initInterval = (
-        measure: number,
-        action: () => void,
-        time: number
-    ) => {
-        if (intervals[measure] == undefined)
-            intervals[measure] = setInterval(action, time)
+    const changeUnit = (u: number) => {
+        data().forEach((u, ui) => stop(ui)) // stop all
+        start(u) // start selected
+        setSelectedUnit(u)
     }
+
+    // // // // measure null will start all measures
+    // // // const start = (unit: number, measure?: number) => {}
 
     // const start = () => {
     //     // code will continually update the selected index of the array.
@@ -123,8 +154,12 @@ function createDataState() {
     // }
 
     return {
-        setUnitCount,
-        setSelected,
+        initialize,
+        changeUnit,
+        selectedUnit,
+        data,
+        // setMachineCount,
+        // setSelected,
         // signalStrength,
         // processorUsage,
         // batteryLevel,
