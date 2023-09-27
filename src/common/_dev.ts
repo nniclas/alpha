a9279974dfaa0c4f7b2cfe08a11e96984bf48c74
nniclas <niclas.oes@gmail.com>
2023-09-27 13:18:22 +0200

    fixed sql query

--- src/common/_dev.ts
+++ src/common/_dev.ts
@@ -2,28 +2,33 @@ import { subDays, subMinutes } from 'date-fns'
 import { randInt } from './utils'
 import { date } from './date-utils'
 
-export const generateSomeStats = (unitIds: number[], count: number) => {
+const backToThePast = new Date('2023-08-14')
+
+export const generateSomeStats = (units: number[], count: number) => {
     const elements = ['Battery', 'Signal strength', 'Processor']
 
     let result = `
+    use alpha;
         INSERT INTO 
             stats(unitid, element, value, date) 
         VALUES 
             `
 
     let someDates = []
-    const today = new Date()
     for (let i = 0; i < count; i++)
-        someDates.push(date(subMinutes(subDays(today, i), randInt(0, 1440))))
+        someDates.push(
+            date(subMinutes(subDays(backToThePast, i), randInt(0, 1440)))
+        )
 
-    for (let e of elements) {
-        for (let i = 0; i < count; i++) {
-            const unitid = unitIds[randInt(0, unitIds.length - 1)]
-            const value = randInt(30, 90) // completely random here....
-            result += `
-            (${unitid}, ${e}, ${value}, ${someDates[i]}")${
-                i < count - 1 ? ',' : ';'
-            } `
+    for (let u of units) {
+        for (let e of elements) {
+            for (let i = 0; i < count; i++) {
+                const value = randInt(30, 90) // completely random here....
+                result += `
+            (${u}, "${e}", ${value}, "${someDates[i]}")${
+                    u != units.length - 1 && i < count - 1 ? ',' : ';'
+                } `
+            }
         }
     }
 
@@ -31,8 +36,8 @@ export const generateSomeStats = (unitIds: number[], count: number) => {
 }
 
 export const generateSomeEntries = (
-    unitIds: number[],
-    userIds: number[],
+    units: number[],
+    users: number[],
     count: number
 ) => {
     const someNotes = [
@@ -47,24 +52,29 @@ export const generateSomeEntries = (
     ]
 
     let result = `
+        use alpha;
         INSERT INTO 
             entries(unitid, userid, event, measure, tag, notes, date) 
         VALUES 
             `
 
     let someDates = []
-    const today = new Date()
     for (let i = 0; i < 30; i++)
         someDates.push(
-            date(subMinutes(subDays(today, randInt(0, 60)), randInt(0, 1440)))
+            date(
+                subMinutes(
+                    subDays(backToThePast, randInt(0, 60)),
+                    randInt(0, 1440)
+                )
+            )
         )
 
     for (let i = 0; i < count; i++) {
-        const unitid = unitIds[randInt(0, unitIds.length - 1)]
+        const unitid = units[randInt(0, units.length - 1)]
 
-        const useridIndex = randInt(0, userIds.length * 4)
+        const useridIndex = randInt(0, users.length * 4)
         const userid =
-            useridIndex <= userIds.length - 1 ? userIds[useridIndex] : 'NULL'
+            useridIndex <= users.length - 1 ? users[useridIndex] : 'NULL'
 
         const event = randInt(1, 5)
 
@@ -79,7 +89,7 @@ export const generateSomeEntries = (
                 ? `"${someNotes[notesIndex]}"`
                 : 'NULL'
 
-        const date = someDates[randInt(0, unitIds.length - 1)]
+        const date = someDates[randInt(0, users.length - 1)]
 
         result += `
             (${unitid}, ${userid}, ${event}, ${measure}, ${tag}, ${notes}, "${date}")${
@@ -94,4 +104,5 @@ export const generateSomeEntries = (
 /////////////// CONSOLE LOG SQL SCRIPTS (comment/uncomment to run/disable)
 //////////////////////////////////////////////////////
 
+console.log(generateSomeStats([1, 2, 3, 4], 30))
 //console.log(generateSomeEntries([1, 2, 3, 4], [1, 2, 3], 30))
